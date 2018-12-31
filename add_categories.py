22d2a770d4ecace97ff05ebcc1e1d7e215eac8a9
Avery <avery.berkowitz@gmail.com>
2018-12-31 15:25:39 -0500

    add user to items

--- add_categories.py
+++ add_categories.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from db_setup import Category, Base, Item
+from db_setup import Category, Base, Item, User
 
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
@@ -9,20 +9,30 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+# Create first user
+User1 = User(name="Avery Berkowitz",
+             email="aman1321@yahoo.com"
+             )
+session.add(User1)
+session.commit()
+
 # First category
 category1 = Category(name="Soccer")
 
 session.add(category1)
 session.commit()
 
-Item1 = Item(name="Soccar Ball", description="You definitely need this in order to play soccer",
+Item1 = Item(user_id=1,
+             name="Soccar Ball",
+             description="You definitely need this in order to play soccer",
              category_name="Soccer", category=category1)
 
 session.add(Item1)
 session.commit()
 
 
-Item2 = Item(name="Shin Guards", description="Protect your legs",
+Item2 = Item(user_id=1,
+             name="Shin Guards", description="Protect your legs",
              category_name="Soccer", category=category1)
 
 session.add(Item2)
@@ -34,14 +44,17 @@ category2 = Category(name="Basketball")
 session.add(category2)
 session.commit()
 
-Item1 = Item(name="basketball", description="You definitely need this in order to play soccer",
+Item1 = Item(user_id=1,
+             name="basketball",
+             description="You definitely need this in order to play soccer",
              category_name="Basketball", category=category2)
 
 session.add(Item1)
 session.commit()
 
 
-Item2 = Item(name="Hoop", description="Usally placed at 10ft",
+Item2 = Item(user_id=1,
+             name="Hoop", description="Usally placed at 10ft",
              category_name="Basketball", category=category2)
 
 session.add(Item2)
@@ -53,14 +66,18 @@ category3 = Category(name="Baseball")
 session.add(category3)
 session.commit()
 
-Item1 = Item(name="baseball", description="You definitely need this in order to play this sport",
+Item1 = Item(user_id=1,
+             name="baseball",
+             description="You definitely need this",
              category_name="Baseball", category=category3)
 
 session.add(Item1)
 session.commit()
 
 
-Item2 = Item(name="Glove", description="Usally placed on your hand, the only necessary equiptment for playing defense",
+Item2 = Item(user_id=1,
+             name="Glove",
+             description="Usally placed on your hand",
              category_name="Baseball", category=category3)
 
 session.add(Item2)
@@ -72,7 +89,9 @@ category4 = Category(name="Frisbee")
 session.add(category4)
 session.commit()
 
-Item1 = Item(name="Frisbee", description="You definitely need this in order to play Frisbee.  Infact, it is all you need",
+Item1 = Item(user_id=1,
+             name="Frisbee",
+             description="You definitely need this in order to play.",
              category_name="Frisbee", category=category4)
 
 session.add(Item1)
@@ -85,14 +104,18 @@ category5 = Category(name="Snowboarding")
 session.add(category5)
 session.commit()
 
-Item1 = Item(name="Down Jacket", description="This will keep you warm on the mountain",
+Item1 = Item(user_id=1,
+             name="Down Jacket",
+             description="This will keep you warm on the mountain",
              category_name="Snowboarding", category=category5)
 
 session.add(Item1)
 session.commit()
 
 
-Item2 = Item(name="Gloves", description="Very Important.  Hands are often the first to get cold in extreme conditions",
+Item2 = Item(user_id=1,
+             name="Gloves",
+             description="Hands are often the first to get cold.",
              category_name="Snowboarding", category=category5)
 
 session.add(Item2)
@@ -104,14 +127,18 @@ category6 = Category(name="Rock Climbing")
 session.add(category6)
 session.commit()
 
-Item1 = Item(name="100m rope", description="Keep yourself or your partner safe as you climb",
+Item1 = Item(user_id=1,
+             name="100m rope",
+             description="Keep yourself or your partner safe as you climb",
              category_name="Rock Climbing", category=category6)
 
 session.add(Item1)
 session.commit()
 
 
-Item2 = Item(name="Rock Climbing Shoes", description="These help your feet stick to the rock",
+Item2 = Item(user_id=1,
+             name="Rock Climbing Shoes",
+             description="These help your feet stick to the rock",
              category_name="Rock Climbing", category=category6)
 
 session.add(Item2)
@@ -123,14 +150,18 @@ category7 = Category(name="Foosball")
 session.add(category7)
 session.commit()
 
-Item1 = Item(name="Table", description="You probably need this to play",
+Item1 = Item(user_id=1,
+             name="Table",
+             description="You probably need this to play",
              category_name="Foosball", category=category7)
 
 session.add(Item1)
 session.commit()
 
 
-Item2 = Item(name="Replacement Balls", description="Very Important.  These can be easily lost",
+Item2 = Item(user_id=1,
+             name="Replacement Balls",
+             description="Very Important.  These can be easily lost",
              category_name="Foosball", category=category7)
 
 session.add(Item2)
@@ -142,7 +173,9 @@ category8 = Category(name="Skating")
 session.add(category8)
 session.commit()
 
-Item1 = Item(name="Skates", description="Made for ice or hard surfaces.  Find out where you will be skating first",
+Item1 = Item(user_id=1,
+             name="Skates",
+             description="Made for ice or hard surfaces.",
              category_name="Skating", category=category8)
 
 session.add(Item1)
@@ -154,14 +187,18 @@ category9 = Category(name="Hockey")
 session.add(category9)
 session.commit()
 
-Item1 = Item(name="Puck", description="You probably need this to play",
+Item1 = Item(user_id=1,
+             name="Puck",
+             description="You probably need this to play",
              category_name="Hockey", category=category9)
 
 session.add(Item1)
 session.commit()
 
 
-Item2 = Item(name="Pads", description="Very Important.  Hockey is a brutal sport and you need to protect your body",
+Item2 = Item(user_id=1,
+             name="Pads",
+             description="You need to protect your body",
              category_name="Hockey", category=category9)
 
 session.add(Item2)
